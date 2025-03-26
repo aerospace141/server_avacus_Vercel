30d36291d6dd3fa67a477ea5755f0241127e27d3
ayush gupta <aerospace141@gmail.com>
2025-03-26 23:51:37 +0530

    Update signup.js

--- Routes/user_auth/signup.js
+++ Routes/user_auth/signup.js
@@ -33,10 +33,10 @@ router.post('/signup', async (req, res) => {
   
       res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
-      if (error.code === 11000) {
-        const field = Object.keys(error.keyValue)[0];
-        return res.status(400).json({ error: `Duplicate value for field: ${field}` });
-      }
+      // if (error.code === 11000) {
+      //   const field = Object.keys(error.keyValue)[0];
+      //   return res.status(400).json({ error: `Duplicate value for field: ${field}` });
+      // }
       console.error('Error creating user:', error);
       res.status(500).json({ error: 'Internal Server Error', message: 'Whole server is not taking off!' });
     }
